e75c5ae451a42e888282f86867b0966545f1693f
Brent-Milan <Bmilan24@gmail.com>
2017-07-18 19:28:24 -0400

    third exercise completed (for loops)

---
+++ src/main/java/production/programmingbydoing/CountingMachine.java
@@ -0,0 +1,22 @@
+package production.programmingbydoing;
+
+import java.util.Scanner;
+
+public class CountingMachine {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Give me an integer to count to: ");
+		int number = input.nextInt();
+		
+		System.out.println("Count to : " + number);
+		
+		for(int count = 0; count <= number; count++) {
+			System.out.println(count);
+		}
+
+	}
+
+}
