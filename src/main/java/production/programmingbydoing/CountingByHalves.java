79c7409301a1b9768d7a37a73b3559eddb660dc4
Brent-Milan <Bmilan24@gmail.com>
2017-07-19 07:21:57 -0400

    fifth exercise complete (for loops)

---
+++ src/main/java/production/programmingbydoing/CountingByHalves.java
@@ -0,0 +1,12 @@
+package production.programmingbydoing;
+
+public class CountingByHalves {
+
+	public static void main(String[] args) {
+		
+		for(double x = -10; x <= 10; x = x + .5)
+			System.out.println(x);
+
+	}
+
+}
