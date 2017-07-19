48d3cc113f2f8abb251d62313fa2af9218e5d87d
Brent-Milan <Bmilan24@gmail.com>
2017-07-19 07:42:31 -0400

    seventh exercise completed

---
+++ src/main/java/production/programmingbydoing/NoticingEvenNumbers.java
@@ -0,0 +1,20 @@
+package production.programmingbydoing;
+
+public class NoticingEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		for(int count = 1; count < 20; count++) {
+			if(count % 2 == 0) {
+				System.out.println(count + "< Notice Me, Senpai");
+			} else	{
+				System.out.println(count);
+			}
+		}
+
+		
+		
+		
+	}
+
+}
--- src/main/java/production/programmingbydoing/XAndY.java
+++ src/main/java/production/programmingbydoing/XAndY.java
@@ -3,10 +3,10 @@ package production.programmingbydoing;
 public class XAndY {
 	public static void main(String[] args) {
 		
-		System.out.println("X\tY");
+		System.out.println(" X\t Y");
 		for(double x = -10; x <= 10; x = x + .5) {
 			double y = x * x;
-			System.out.println("---\t---");
+			System.out.println("----\t----");
 			System.out.println(x + "\t" + y);
 
 	}
