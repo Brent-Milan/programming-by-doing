7bd0a8cb359b733732b138b42d6afbb8de7c93a5
Brent-Milan <Bmilan24@gmail.com>
2017-07-19 07:32:29 -0400

    sixth exercise complete (for loops)

---
+++ src/main/java/production/programmingbydoing/XAndY.java
@@ -0,0 +1,16 @@
+package production.programmingbydoing;
+
+public class XAndY {
+	public static void main(String[] args) {
+		
+		System.out.println("X\tY");
+		for(double x = -10; x <= 10; x = x + .5) {
+			double y = x * x;
+			System.out.println("---\t---");
+			System.out.println(x + "\t" + y);
+
+	}
+
+}
+
+}
