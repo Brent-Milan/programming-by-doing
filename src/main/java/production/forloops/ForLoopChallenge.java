10b35062c758a1368d71e7ed710b7ec293b27bd3
Brent-Milan <Bmilan24@gmail.com>
2017-07-22 22:46:21 -0400

    for loop challenge complete

---
+++ src/main/java/production/forloops/ForLoopChallenge.java
@@ -0,0 +1,14 @@
+package production.forloops;
+
+public class ForLoopChallenge {
+
+	public static void main(String[] args) {
+		
+		int x = 3;
+		for(int count = 15; count <= 30; count += x) {
+			System.out.println(count);
+		}
+
+	}
+
+}
