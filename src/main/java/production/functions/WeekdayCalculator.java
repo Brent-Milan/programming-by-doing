9f0ddf6d280e43ef6eef47e07a6158cd7a9e71f2
Brent-Milan <Bmilan24@gmail.com>
2017-08-07 20:21:29 -0400

    steps 1-6 done on WeekdayCalculator

--- src/main/java/production/functions/WeekdayCalculator.java
+++ src/main/java/production/functions/WeekdayCalculator.java
@@ -1,3 +1,4 @@
+package production.functions;
 import java.util.Scanner;
 
 public class WeekdayCalculator
@@ -34,7 +35,14 @@ public class WeekdayCalculator
 	public static String weekday( int mm, int dd, int yyyy )
 	{
 		int yy = yyyy - 1900;
-		int total = (yy / 4) + yy + dd;
+		int x = month_offset(mm);
+		int total = (yy / 4) + yy + dd + x;
+		boolean leapYear = is_leap(yyyy);
+			if(leapYear == true && mm == 1 || leapYear == true && mm == 2) {
+				total -= 1;
+			}
+		int remainder = total / 7;
+		
 		String date = "";
 
 		// bunch of calculations go here
@@ -46,6 +54,71 @@ public class WeekdayCalculator
 
 
 	// paste your functions from MonthName, WeekdayName, and MonthOffset here
+	public static String month_name(int monthNum) {
+		if(monthNum == 1) {
+			return "January";			
+		} else if (monthNum == 2) {
+			return "February";
+		} else if (monthNum == 3) {
+			return "March";
+		} else if (monthNum == 4) {
+			return "April";
+		} else if (monthNum == 5) {
+			return "May";
+		} else if (monthNum == 6) {
+			return "June";
+		} else if (monthNum == 7) {
+			return "July";
+		} else if (monthNum == 8) {
+			return "August";
+		} else if (monthNum == 9) {
+			return "September";
+		} else if (monthNum == 10) {
+			return "October";
+		} else if (monthNum == 11) {
+			return "November";
+		} else if (monthNum == 12) {
+			return "December";
+		} else {
+			return "Nonsense. Try again";			
+		}
+	}
+
+	public static int month_offset( int month ) {
+		int result;
+		switch(month) {
+			case 1: result = 1;
+					break;
+			case 2: result = 4;
+					break;
+			case 3: result = 4;
+					break;
+			case 4: result = 0;
+					break;
+			case 5: result = 2;
+					break;
+			case 6: result = 5;
+					break;
+			case 7: result = 0;
+					break;
+			case 8: result = 3;
+					break;
+			case 9: result = 6;
+					break;
+			case 10: result = 1;
+					break;
+			case 11: result = 4;
+					break;
+			case 12: result = 6;
+					break;
+			default: result = -1;
+		}
+		return result;
+	}
+	
+	public String weekdayName(int ) {
+		switch
+	}
 		
 	public static boolean is_leap( int year )
 	{
