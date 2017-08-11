d16f8cf8153a61757d70133f0311abfc9c0fc5c0
Brent-Milan <Bmilan24@gmail.com>
2017-08-11 16:38:35 -0400

    exercise complete with very specific print output and notes

--- src/main/java/production/arrays/CopyingArrays.java
+++ src/main/java/production/arrays/CopyingArrays.java
@@ -11,7 +11,9 @@ public class CopyingArrays {
 		int[] arrayOne = new int[10];
 		int[] arrayTwo = new int[10];
 		
+		//Populates and prints arrayOne
 		System.out.println("arrayOne contains the following elements:");
+		
 		for(int index = 0; index < 10; index++) {
 			int randomNum = 1 + r.nextInt(99);
 			arrayOne[index] = randomNum;
@@ -19,15 +21,25 @@ public class CopyingArrays {
 		}
 
 		
-		//ArrayOne copied to ArrayTwo
+		//arrayOne copied to arrayTwo
 		System.arraycopy(arrayOne, 0, arrayTwo, 0, 10);
 		
 		//Testing whether copy occurred
 		System.out.println("\narrayTwo contains the following elements: ");
+		
 		for(int index = 0; index < arrayTwo.length; index++) {
 			System.out.print(arrayTwo[index] + "  ");
 		}
 		
+		
+		//Changing final index on arrayOne per instructions
+		arrayOne[9] = -7;
+		
+		System.out.println("\narrayOne contains the following elements after adjustment: ");
+		
+		for(int index = 0; index < arrayOne.length; index++) {
+			System.out.print(arrayOne[index] + "  ");
+		}
 		
 	}
 
