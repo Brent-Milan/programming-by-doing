33f8f383daa2dc4ee4ab42de931c830c8e60b798
Brent-Milan <Bmilan24@gmail.com>
2017-08-10 22:31:16 -0400

    BasicArrays3 started

---
+++ src/main/java/production/arrays/BasicArrays3.java
@@ -0,0 +1,19 @@
+package production.arrays;
+
+import java.util.Random;
+
+public class BasicArrays3 {
+
+	public static void main(String[] args) {
+		
+			Random r = new Random();
+			
+			int randomNum = 10 + r.nextInt(99);
+			
+			int[] anArray = new int[1000];
+			
+			
+			
+	}
+
+}
