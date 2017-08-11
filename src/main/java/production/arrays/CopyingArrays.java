bafe6371d107653bb8a0748b74df39c8e9edc555
Brent-Milan <Bmilan24@gmail.com>
2017-08-11 11:07:18 -0400

    array population compelete

--- src/main/java/production/arrays/CopyingArrays.java
+++ src/main/java/production/arrays/CopyingArrays.java
@@ -1,16 +1,20 @@
 package production.arrays;
 
+import java.util.Random;
+
 public class CopyingArrays {
 
 	public static void main(String[] args) {
 		
 		Random r = new Random();
-		int randomNum = 1 + r.nextInt(99);
 		
 		int[] arrayOne = new int[10];
 		
+		System.out.println("arrayOne contains the following elements:");
 		for(int index = 0; index < 10; index++) {
+			int randomNum = 1 + r.nextInt(99);
 			arrayOne[index] = randomNum;
+			System.out.print(arrayOne[index] + " ");
 		}
 
 	}
