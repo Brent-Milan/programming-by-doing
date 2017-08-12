24512c19bfda3252b24d5a3a99c99dd943d853f9
Brent-Milan <Bmilan24@gmail.com>
2017-08-12 09:45:53 -0400

    implementing PrintWriter

--- src/main/java/production/printing/LetterRevisited.java
+++ src/main/java/production/printing/LetterRevisited.java
@@ -1,9 +1,19 @@
 package production.printing;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class LetterRevisited {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		String fileName = "PBD_letter.text";
+		String[] anArrayOfStrings = new String[12];
+		
+		PrintWriter fileWriter = new PrintWriter(new FileWriter(fileName));
+		
+	
 		System.out.println("+--------------------------------------------+");
 		System.out.println("|                                       #### |");
 		System.out.println("|                                       #### |");
@@ -17,6 +27,11 @@ public class LetterRevisited {
 		System.out.println("|                                            |");
 		System.out.println("+--------------------------------------------+");	
 		
+		}
 		
 	}
+	
+	
+	
+	fileWriter.close();
 }
