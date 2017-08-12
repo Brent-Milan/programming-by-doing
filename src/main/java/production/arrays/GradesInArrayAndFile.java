85ac54c366f8efa02a2087ea1a0674897104c79d
Brent-Milan <Bmilan24@gmail.com>
2017-08-12 15:31:29 -0400

    fileWriter closed out, notes added, order flow tidied

--- src/main/java/production/arrays/GradesInArrayAndFile.java
+++ src/main/java/production/arrays/GradesInArrayAndFile.java
@@ -15,8 +15,7 @@ public class GradesInArrayAndFile {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int[] gradesInArray = new int[5];
-		
+		//Collects user input to store in file
 		System.out.println("Enter the first name of the student: ");
 		String studentFirst = input.next();
 		
@@ -26,24 +25,36 @@ public class GradesInArrayAndFile {
 		System.out.println("Enter the name of the save file: ");
 		String studentFile = input.next();
 		
+		PrintWriter fileWriter = new PrintWriter(new FileWriter(studentFile));
 		
+		//Creates array for student grades
+		int[] gradesInArray = new int[5];
 		
+		//Populates that array with random numbers ("grades")
 		for(int index = 0; index < 5; index++) {
 			int randomNum = 1 + r.nextInt(99);
 			gradesInArray[index] = randomNum;
 		}
 
-		System.out.println("File saved under: " + studentFile);
-		PrintWriter fileWriter = new PrintWriter(new FileWriter(studentFile));
+		
+		//Prints to console name of file 
+		System.out.println("\nFile saved under: " + studentFile);
+		
+		//Prints student name to console + saves full name in array
+		/**************************************************************/
 		
 		System.out.println("\nUser profile: " + studentFirst + " " + studentLast);
-		String[] nameArray = { studentFirst, studentLast };
 		
+		String[] nameArray = { "First Name: " + studentFirst, "Last name: " + studentLast };
 		
 		for(int index = 0; index < nameArray.length; index++) {
 			fileWriter.print(nameArray[index]);
 		}
 		
+		/************************************************************/
+		
+		
+		//Prints grades to console + prints grades to file
 		System.out.println("Grades for this session are: ");
 		
 		for(int index = 0; index < gradesInArray.length; index++) {
@@ -56,6 +67,6 @@ public class GradesInArrayAndFile {
 		
 		
 		
-	}
+	} // end main
 
 }
