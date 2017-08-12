253a325741bc5f4885f292de2d8c021657dc2285
Brent-Milan <Bmilan24@gmail.com>
2017-08-12 09:22:36 -0400

    ALetterToYourself started

---
+++ src/main/java/production/printing/ALetterToYourself.java
@@ -0,0 +1,16 @@
+package production.printing;
+
+public class ALetterToYourself {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+------------------------------------------+");
+		System.out.println("                                       #### ");
+		System.out.println("                                       #### ");
+		System.out.println("                                       #### ");
+		System.out.println("+------------------------------------------+");	
+		
+		
+	}
+
+}
