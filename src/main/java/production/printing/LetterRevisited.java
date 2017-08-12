e329f9ddd5a71cbec15e87e5608c850166a4a51a
Brent-Milan <Bmilan24@gmail.com>
2017-08-12 09:51:30 -0400

    'letter' saved into String[]

--- src/main/java/production/printing/LetterRevisited.java
+++ src/main/java/production/printing/LetterRevisited.java
@@ -9,29 +9,32 @@ public class LetterRevisited {
 	public static void main(String[] args) throws IOException {
 		
 		String fileName = "PBD_letter.text";
-		String[] anArrayOfStrings = new String[12];
+		String[] anArrayOfStrings = {
 		
-		PrintWriter fileWriter = new PrintWriter(new FileWriter(fileName));
 		
 	
-		System.out.println("+--------------------------------------------+");
-		System.out.println("|                                       #### |");
-		System.out.println("|                                       #### |");
-		System.out.println("|                                       #### |");
-		System.out.println("|                                            |");
-		System.out.println("|                                            |");
-		System.out.println("|                                            |");
-		System.out.println("|                         Bill Gates         |");
-		System.out.println("|                         1 Microsoft Way    |");
-		System.out.println("|                         Redmond, WA 98104  |");
-		System.out.println("|                                            |");
-		System.out.println("+--------------------------------------------+");	
+		"+--------------------------------------------+",
+		"|                                       #### |",
+		"|                                       #### |",
+		"|                                       #### |",
+		"|                                            |",
+		"|                                            |",
+		"|                                            |",
+		"|                         Bill Gates         |",
+		"|                         1 Microsoft Way    |",
+		"|                         Redmond, WA 98104  |",
+		"|                                            |",
+		"+--------------------------------------------+"	
 		
 		}
 		
-	}
+		PrintWriter fileWriter = new PrintWriter(new FileWriter(fileName));
+		
+		
+		fileWriter.close();
+		
+		}
+		
 	
 	
-	
-	fileWriter.close();
 }
