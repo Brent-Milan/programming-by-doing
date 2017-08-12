08c6fcfac574ee03b9bff997f7d9ac7f43129931
Brent-Milan <Bmilan24@gmail.com>
2017-08-11 22:44:11 -0400

    GradesInArray started

---
+++ src/main/java/production/arrays/GradesInArrayAndFile.java
@@ -0,0 +1,19 @@
+package production.arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GradesInArrayAndFile {
+
+	public static void main(String[] args) {
+		
+		
+		Random r = new Random();
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+
+	}
+
+}
