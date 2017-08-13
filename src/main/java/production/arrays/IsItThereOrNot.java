6e865920e439c66e66c8145c43351344f3fbe833
Brent-Milan <Bmilan24@gmail.com>
2017-08-13 14:00:18 -0400

    IsItThereOrNot exercise complete

--- src/main/java/production/arrays/IsItThereOrNot.java
+++ src/main/java/production/arrays/IsItThereOrNot.java
@@ -20,6 +20,29 @@ public class IsItThereOrNot {
 			System.out.print(anArray[index] + "  ");
 		}
 
+		System.out.println("\n\nWhat number would you like to validate?");
+		int userInput = input.nextInt();
+		
+		int count = 0;
+		
+		//increments count variable if a matching value is found
+		for(int index = 0; index < anArray.length; index++) {
+			if(anArray[index] == userInput) {
+				count++;
+			}
+		}
+		
+		
+		//determines whether a matching value was found in the previous for loop
+		if(count > 0) {
+			System.out.println("\n" + userInput + " is present in the array.");
+		} else if(count == 0) {
+			System.out.println("\nThe array does not contain " + userInput + "n");
+		}
+		
+		
+		
+		
 	}
 
 }
