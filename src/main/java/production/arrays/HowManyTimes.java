0b7a1d8a5cdfa175900189ddae926d5d6a6044af
Brent-Milan <Bmilan24@gmail.com>
2017-08-13 12:16:47 -0400

    user input implemented, array populates and prints

--- src/main/java/production/arrays/HowManyTimes.java
+++ src/main/java/production/arrays/HowManyTimes.java
@@ -1,15 +1,33 @@
 package production.arrays;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HowManyTimes {
 
 	public static void main(String[] args) {
 		
 		
 		Random r = new Random();
-		
 		Scanner input = new Scanner(System.in);
 		
+		
 		int[] anArray = new int[10];
+		
+		System.out.print("Array: ");
+		
+		//populates array with random numbers + prints out array elements
+		for(int index = 0; index < anArray.length; index++) {
+			anArray[index] = 1 + r.nextInt(49);
+			System.out.print(anArray[index] + "  ");
+		}
+		
+		System.out.println("\nWhich number would you like to check the frequency of?");
+		int userInput = input.nextInt();
+		
+		
+		
+		
 
 	}
 
