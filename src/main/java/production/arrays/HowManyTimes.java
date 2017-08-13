f90672f6b0234f7f53282da182a3eca2489722a3
Brent-Milan <Bmilan24@gmail.com>
2017-08-13 12:07:40 -0400

    HowManyTimes exercise started, necessary imports imp

---
+++ src/main/java/production/arrays/HowManyTimes.java
@@ -0,0 +1,16 @@
+package production.arrays;
+
+public class HowManyTimes {
+
+	public static void main(String[] args) {
+		
+		
+		Random r = new Random();
+		
+		Scanner input = new Scanner(System.in);
+		
+		int[] anArray = new int[10];
+
+	}
+
+}
