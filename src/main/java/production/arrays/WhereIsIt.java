71ea311c29f0897f866039080cda756284bc0b6d
Brent-Milan <Bmilan24@gmail.com>
2017-08-14 13:18:35 -0400

    loop established to check userinput, print matching index pos

--- src/main/java/production/arrays/WhereIsIt.java
+++ src/main/java/production/arrays/WhereIsIt.java
@@ -11,12 +11,32 @@ public class WhereIsIt {
 		
 		Random r = new Random();
 		
-		int[] anArray = int[10];
+		int[] anArray = new int[10];
+		
+		System.out.print("Array: ");
 		
 		for(int index = 0; index < anArray.length; index++) {
 			anArray[index] = 1 + r.nextInt(49);
+			System.out.print(anArray[index] + "  ");
 		}
 
+		
+		System.out.println("\n\nWhich number would you like to validate?");
+		int userInput = input.nextInt();
+		
+		int count = 0;
+		for(int index = 0; index < anArray.length; index++) {
+			if(userInput == anArray[index]) {
+				System.out.println("Found " + userInput + " at index " + index);
+				count++;
+			}
+			
+		}
+		
+		
+		
+		System.out.println("Your number was found " + count + "time(s).");
+		
 	}
 
 }
