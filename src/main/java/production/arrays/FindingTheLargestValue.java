b50964c4b7d009f3a13522d10118d93bf1b8ed1b
Brent-Milan <Bmilan24@gmail.com>
2017-08-14 16:24:49 -0400

    FindingTheLargestValue completed

--- src/main/java/production/arrays/FindingTheLargestValue.java
+++ src/main/java/production/arrays/FindingTheLargestValue.java
@@ -1,6 +1,7 @@
 package production.arrays;
 
-import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.Scanner;
 
 public class FindingTheLargestValue {
 
@@ -13,10 +14,25 @@ public class FindingTheLargestValue {
 		
 		int[] anArray = new int[10];
 		
-		for(int index = 10; index < anArray.length; index++) {
+		System.out.print("Array: ");
+		
+		for(int index = 0; index < anArray.length; index++) {
 			anArray[index] = 1 + r.nextInt(99);
+			System.out.print(anArray[index] + "  ");
 		}
 
-	}
+		
+		int theOne = 0;
+		
+		for(int index = 0; index < anArray.length; index++) {
+			if(anArray[index] > theOne) {
+				theOne = anArray[index];
+			}
+		}
+	
+		System.out.println("\nThe largest value in the array is " + theOne);
+		
+		
+	}  //end main
 
 }
